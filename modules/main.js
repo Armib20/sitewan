49b8a824e5b8142727ae0bbeeee524c9ebc59e5b
Iris Xu <irisxu02@gmail.com>
2024-08-17 19:44:39 -0700

    Fix indentations and change var to let

---
+++ modules/main.js
@@ -0,0 +1,6 @@
+import './modes.js'; // defines the different modes that the app can run under
+import './rubik.js'; // creates the cube and adds to the scene
+import './face_rotation.js'; // defines motions for each face rotation and handles its animation
+import './canvas.js';
+import './action_utils.js';
+import './solve_cube.js';
--- motion.js
+++ motion.js
@@ -4,49 +4,49 @@
 
 class Keyframe {
     constructor(name,time,avars) {
-    this.name = name;
-    this.time = time;
-    this.avars = avars;
+        this.name = name;
+        this.time = time;
+        this.avars = avars;
     }
 }
 
 class Motion {
     constructor(setMatricesFunc) {
-    this.keyFrameArray = [];          // list of keyframes
-    this.maxTime = 0.0;               // time of last keyframe
-    this.currTime = 0.0;              // current playback time
-    this.updateMatrices = setMatricesFunc;    // function to call to update transformation matrices
+        this.keyFrameArray = [];          // list of keyframes
+        this.maxTime = 0.0;               // time of last keyframe
+        this.currTime = 0.0;              // current playback time
+        this.updateMatrices = setMatricesFunc;    // function to call to update transformation matrices
     };
     reset() {                     // go back to first keyframe
-    this.currTime = 0.0;
+        this.currTime = 0.0;
     };
     addKeyFrame(keyframe) {               // add a new keyframe at end of list
-    this.keyFrameArray.push(keyframe);
-    if (keyframe.time > this.maxTime)
-        this.maxTime = keyframe.time;
+        this.keyFrameArray.push(keyframe);
+        if (keyframe.time > this.maxTime)
+            this.maxTime = keyframe.time;
     };
     timestep(dt) {                //  take a time-step;  loop to beginning if at end
-    this.currTime += dt;
-    if (this.currTime > this.maxTime) 
-        this.currTime = 0;
-    var avars = this.getAvars();
-    this.updateMatrices(avars);
+        this.currTime += dt;
+        if (this.currTime > this.maxTime) 
+            this.currTime = 0;
+        let avars = this.getAvars();
+        this.updateMatrices(avars);
     };
     getAvars() {                  //  compute interpolated values for the current time
-    var i = 1;
-    while (this.currTime > this.keyFrameArray[i].time)       // find the right pair of keyframes
-        i++;
-    var avars = [];
-    for (var n=0; n<this.keyFrameArray[i-1].avars.length; n++) {   // interpolate the values
-        var y0 = this.keyFrameArray[i-1].avars[n];
-        var y1 = this.keyFrameArray[i].avars[n];
-        var x0 = this.keyFrameArray[i-1].time;
-        var x1 = this.keyFrameArray[i].time;
-        var x = this.currTime;
-        var y = y0 + (y1-y0)*(x-x0)/(x1-x0);    // linearly interpolate
-        avars.push(y);
-    }
-    return avars;         // return list of interpolated avars
+        let i = 1;
+        while (this.currTime > this.keyFrameArray[i].time)       // find the right pair of keyframes
+            i++;
+        let avars = [];
+        for (let n=0; n<this.keyFrameArray[i-1].avars.length; n++) {   // interpolate the values
+            let y0 = this.keyFrameArray[i-1].avars[n];
+            let y1 = this.keyFrameArray[i].avars[n];
+            let x0 = this.keyFrameArray[i-1].time;
+            let x1 = this.keyFrameArray[i].time;
+            let x = this.currTime;
+            let y = y0 + (y1-y0)*(x-x0)/(x1-x0);    // linearly interpolate
+            avars.push(y);
+        }
+        return avars;         // return list of interpolated avars
     };
 }
 
